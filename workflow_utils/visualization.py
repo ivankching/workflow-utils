34818a8fe2c52d0036cc634484efe166d1e5ae8e
ivankching <ivan1335@gmail.com>
2024-10-03 11:02:14 +0800

    Add optional parameters for histograms

--- workflow_utils/visualization.py
+++ workflow_utils/visualization.py
@@ -2,6 +2,6 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
-def histograms(data: pd.DataFrame):
+def histograms(data: pd.DataFrame, size: tuple = (16,20), bins: int = 50):
     data = data.select_dtypes(include=[np.int64, np.float64])
-    return data.hist(figsize=(16,20), xlabelsize=7, ylabelsize=7, bins=50)
+    return data.hist(figsize=size, xlabelsize=7, ylabelsize=7, bins=bins)
