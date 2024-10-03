5de2a403412f6c5d95196800a74cb1b70540c8cf
ivankching <ivan1335@gmail.com>
2024-10-03 11:21:54 +0800

    Add test for kdes

--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -6,3 +6,8 @@ def test_histograms():
     data = sns.load_dataset('iris')
     _ = visualization.histograms(data)
     assert True
+
+def test_kdes():
+    data = sns.load_dataset('iris')
+    _ = visualization.kdes(data)
+    assert True
