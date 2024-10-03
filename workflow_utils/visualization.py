61a139afbd35a137fc839f9aa19c894d136e6c81
ivankching <ivan1335@gmail.com>
2024-10-03 11:19:37 +0800

    Add kde plots for numeric fields

--- workflow_utils/visualization.py
+++ workflow_utils/visualization.py
@@ -3,5 +3,11 @@ import pandas as pd
 import seaborn as sns
 
 def histograms(data: pd.DataFrame, size: tuple = (16,20), bins: int = 50):
+    """Return histograms of all numeric fields in a grid"""
     data = data.select_dtypes(include=[np.int64, np.float64])
     return data.hist(figsize=size, xlabelsize=7, ylabelsize=7, bins=bins)
+
+def kdes(data: pd.DataFrame, size: tuple = (16,20)):
+    """Return KDE plots of all numeric fields stacked vertically"""
+    data = data.select_dtypes(include=[np.int64, np.float64])
+    return data.plot.kde(figsize=size, subplots=True)
