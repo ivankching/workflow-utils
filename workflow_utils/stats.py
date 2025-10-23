acbf7426cac1db231023fa0af914b8b9c1fb245d
Ivan Ching <ivan1335@gmail.com>
2025-10-23 11:03:40 +0800

    Add KS function

--- workflow_utils/stats.py
+++ workflow_utils/stats.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from statsmodels.stats.outliers_influence import variance_inflation_factor
+from scipy.stats import kstest
 
 
 def vif(data, exclude=None):
@@ -27,3 +28,39 @@ def vif(data, exclude=None):
     vif_data["VIF"] = [variance_inflation_factor(data.values, i) for i in range(data.shape[1])]
     vif_data = vif_data.sort_values(by="VIF", ascending=False)
     return vif_data
+
+def kstest(data, label, columns='numeric'):
+    """
+    Calculate the Kolmogorov-Smirnov test statistic and p-value for each
+    feature in the dataset.
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        The dataset to calculate the test statistic and p-value for.
+    label : str
+        The label to split the data into two groups [0, 1].
+    columns : list or str, optional
+        Either 'numeric' to select all numeric features, or a list of
+        feature names to select.
+
+    Returns
+    -------
+    pd.DataFrame
+        A DataFrame containing the test statistic and p-value for each
+        feature in the dataset.
+    """
+    if columns is 'numeric':
+        columns = data.select_dtypes(include='number').columns
+    zeros = data[data[label] == 0][columns]
+    ones = data[data[label] == 1][columns]
+
+    statistics = []
+    pvalues = []
+
+    for col in columns:
+        res = kstest(zeros[col], ones[col])
+        statistics.append(res.statistic)
+        pvalues.append(res.pvalue)
+
+    return pd.DataFrame({'feature': columns, 'statistic': statistics, 'pvalue': pvalues})
